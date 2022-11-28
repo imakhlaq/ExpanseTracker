6a4fd0bfd7a27da37a409b3e47978a30a7c0a997
imakhlaq <imakhlaqXD@gmail.com>
2022-11-28 12:52:08 +0530

    2nd

--- src/components/ExpanseItem.js
+++ src/components/ExpanseItem.js
@@ -1,4 +1,18 @@
+import "./ExpanseItem.css";
+
 function ExpanceItem() {
-  return <p>hello</p>;
+  const expanceDate = new Date(2021, 2, 28);
+  const expanceType = "car Repair";
+  const expnaceCost = 323.32;
+
+  return (
+    <div className="expense-item">
+      <div>{expanceDate.toDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{expanceType}</h2>
+        <div className="expense-item__price">$ {expnaceCost} </div>
+      </div>
+    </div>
+  );
 }
 export default ExpanceItem;

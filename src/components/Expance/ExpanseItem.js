270242ba7a54555f5fce254695411f0fd4741b76
imakhlaq <imakhlaqXD@gmail.com>
2022-11-28 21:45:12 +0530

    v5

--- src/components/Expance/ExpanseItem.js
+++ src/components/Expance/ExpanseItem.js
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpanceDate from "./ExpanceDate";
 import "./ExpanseItem.css";
 
 function ExpanceItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("update");
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       <ExpanceDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">$ {props.amount} </div>
       </div>
+      <button onClick={clickHandler}>click Here</button>
     </Card>
   );
 }
